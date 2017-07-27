0b73d3ea4e0baf25c971577057285574a352700e
John Passaro <john.a.passaro@gmail.com>
2017-07-27 14:03:08 -0400

    make --fmt default to wav and clean up cli logic

--- waveform_gen/cli.py
+++ waveform_gen/cli.py
@@ -18,6 +18,7 @@ def validate_bit_depth(ctx, param, value):
 
 def make_command(f, odd=True):
     cls = OddPeriodicSignal if odd else PeriodicSignal
+
     @cli.command()
     @click.option('--ffmpeg-params', nargs='+')
     @click.option('-b', '--bit-depth', type=int,
@@ -28,29 +29,32 @@ def make_command(f, odd=True):
     @click.option('--volume', type=float, help='volume in dB', default=0.0)
     @click.option('-d', '--duration', type=float,
                   help='length of sample (millis)', default=1000.0)
-    @click.option('--fmt', help='audio format', default=None)
+    @click.option('--fmt', default=None, help=(
+        'audio format. If --out is given, value is inferred, otherwise it'
+        ' defaults to wav. Formats other than wav require ffmpeg to be'
+        ' installed, with proper support.'
+    ))
     @click.option('-o', '--out', type=click.Path(
         dir_okay=False, writable=True, readable=False, resolve_path=True
     ), help=(
-        'output file. Must have a valid format extension. If omitted, --fmt'
-        ' must be specified, and the file is written in /tmp.'
+        'output file. Must have a valid format extension, see --fmt. If'
+        ' omitted, a file name is created based on waveform name,'
+        ' frequency, and --fmt (default wav), and the file is written in /tmp.'
     ))
     @click.argument('frequency', type=float, metavar='FREQUENCY_IN_HZ')
     @functools.wraps(f)
     def inner(frequency, out, fmt, duration, volume, sample_rate,
               bit_depth, ffmpeg_params):
-        if fmt is None:
-            if out:
-                fmt = os.path.splitext(out)
+        if out is None:
+            fmt = fmt or 'wav'
+            out = '/tmp/%s_%d.%s' % (cls.__name__, int(frequency), fmt)
+        elif fmt is None:
+            fmt = os.path.splitext(out)
             if not fmt:
                 raise click.BadParameter('Unable to determine format.',
                                          param_hint=('fmt', 'out'))
-        if not out:
-            out = '/tmp/%s_%d.%s' % (cls.__name__, int(frequency), fmt)
 
         gen = cls(frequency, f, sample_rate=sample_rate, bit_depth=bit_depth)
         gen.to_audio_segment(duration=duration, volume=volume).export(
             click.open_file(out, 'wb+'), format=fmt, parameters=ffmpeg_params)
         click.echo('wrote sample to %s' % out)
-
-
